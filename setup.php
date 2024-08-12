8222507fcbb5c436a9a3eb33a3c317d242c6b351
minzords <florian.blanchet@itsm-ng.com>
2024-08-12 09:34:03 +0200

    refactor: change author and link

--- setup.php
+++ setup.php
@@ -62,8 +62,8 @@ function plugin_version_onetimesecret() {
 	return [
 		'name'=> 'OneTimeSecret',
 		'version' => PLUGIN_ONETIMESECRET_VERSION,
-		'author' => '<a href="https://tic.gal">TICgal</a>',
-		'homepage' => 'https://tic.gal',
+		'author' => 'TICgal, ITSM-NG',
+		'homepage' => 'https://github.com/itsmng/one-timesecret',
 		'license' => 'GPLv3+',
 		'minGlpiVersion' => PLUGIN_ONETIMESECRET_MIN_GLPI,
 		'requirements' => [
